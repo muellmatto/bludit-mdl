2858c17ad9e646dffd62750f4ef6d8366d36bfbd
matto <matto@damniam.de>
2016-08-25 13:43:48 +0200

    --last commit

--- php/home.php
+++ php/home.php
@@ -68,8 +68,8 @@
             <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet">
                 <div class="seht-card mdl-card mdl-shadow--4dp">
 
-                    <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">
+                    <div class="mdl-card__title" style="background-color: rgb(0,68,148); height: 120px;">
+                        <h2 class="mdl-card__title-text" style="font-size: 32px; color: white;">
                             <?php echo $Post->title() ?>
                         </h2>
                     </div>
